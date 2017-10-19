35a4981cf8111265ba2a61831b0b3645f554e8a1
Wan Zulkarnain <wan@wanzul-hosting.com>
2017-10-19 13:11:03 +0800

    TestAtomicInteger

---
+++ TestAtomicInteger1p.java
@@ -0,0 +1,51 @@
+package javaaplication5;
+
+class TestAtomicInteger1p {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        CountProblem pt = new CountProblem();
+        Thread t1 = new Thread(pt, "t1");
+        Thread t2 = new Thread(pt, "t2");
+        t1.start();
+
+        // Using busy-waiting techniques
+        while (pt.getCount() != 5) {
+        }
+
+        t2.start();
+
+        // Using busy-waiting techniques
+        while (pt.getCount() != 10) {
+        }
+
+        t1.join();
+        t2.join();
+        System.out.println("Count=" + pt.getCount());
+    }
+}
+
+class CountProblem implements Runnable {
+
+    private int count;
+
+    @Override
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            processSomething(i);
+            count++;
+        }
+    }
+
+    public int getCount() {
+        return this.count;
+    }
+
+    private void processSomething(int i) {
+        try {
+            Thread.sleep(i * 200);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
